f19e5d6bb472ed0ff73b1a669f844a08da265b25
hughweber24 <hughweber24@gmail.com>
2026-04-22 15:13:08 +0100

    Implement User class with constructors and display method
    Defines User class with constructors, destructor, and method to display user details.

---
+++ User.cpp
@@ -0,0 +1,25 @@
+#include "User.h"
+
+User::User() {  // defines default constructor
+    userId = 0;
+    username = "unknown";
+    password = "";
+    email = "";
+}
+
+User::User(int id, string u, string p, string e) { // defines parameterized constructor
+    userId = id;
+    username = u;
+    password = p;
+    email = e;
+}
+
+User::~User() {} // defines virtual destructor
+
+void User::displayDetails() { // defines method to display user details
+    cout << "User: " << getUsername() << " | Email: " << getEmail()
+         << " | Role: " << getRole() << endl; // getRole is virtual
+}
+
+bool operator==(const User& a, const User& b) {
+    return (a.username == b.username);
